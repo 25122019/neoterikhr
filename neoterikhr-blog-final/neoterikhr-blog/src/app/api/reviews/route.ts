3e3500d295338090873a626fcc30d111b7713a57
xphoenix <94502718+25122019@users.noreply.github.com>
2026-02-09 17:25:54 +0700

    Delete neoterikhr-blog-final/neoterikhr-blog directory

--- neoterikhr-blog-final/neoterikhr-blog/src/app/api/reviews/route.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "@/lib/prisma";
-import { seedReviews } from "@/lib/data";
-
-export async function GET() {
-  await seedReviews();
-  const reviews = await prisma.review.findMany();
-  return Response.json(reviews);
-}
